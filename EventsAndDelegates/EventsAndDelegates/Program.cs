f38bc1ef85bbe2501375626804e9c8744ed26f5c
ChristianWoeber <christianwoeber@gmx.at>
2016-02-07 11:06:59 +0100

    Events and Delegates (Video Encoder with Events and Delegates)

---
+++ EventsAndDelegates/EventsAndDelegates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vid = new Video { Title = "Terminator" };
+            var vidEncoder = new VideoEncoder();//Publisher
+            var sms = new SmsNotification();//Subscriber
+            var mail = new MailNotification();//Subscriber
+
+            vidEncoder.VideoEncoded += sms.OnVideoEncoded; //Register the event
+            vidEncoder.VideoEncoded += mail.OnVideoEncoded; //Register the event
+
+            vidEncoder.Encode(vid);
+
+        }
+
+        
+    }
+}

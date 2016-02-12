5841a5ec1f070ddef01955f9781edc5700ba0b3b
ChristianWoeber <christianwoeber@gmx.at>
2016-02-12 15:38:18 +0100

    Cntrl StopWatch Output hinzugefügt

--- StopWatch/Controls/ControlStopWatch.xaml.cs
+++ StopWatch/Controls/ControlStopWatch.xaml.cs
@@ -35,7 +35,7 @@ namespace StopWatch.Controls
             _timer = new DispatcherTimer();
             _viewModel = new StopWatchViewModel();
             _timer.Interval = new TimeSpan(0, 0, 0, 0, 16);
-            lblAusgabe.Content = "00:00:00";
+          //  lblAusgabe.Content = "00:00:00";
             _timer.Tick += UpdateWatch;
             DataContext = _viewModel;
             //lstView.ItemsSource = _viewModel.LstRoundTimes; 
@@ -44,7 +44,10 @@ namespace StopWatch.Controls
 
         private void UpdateWatch(object sender, EventArgs e)
         {
-            lblAusgabe.Content = _viewModel.CurrentTime.ToString(@"mm\:ss\:ff");
+            //lblAusgabe.Content = _viewModel.CurrentTime.ToString(@"mm\:ss\:ff");
+            cntlStopWatchOutput.txtMM.Text = _viewModel.CurrentTime.Minutes.ToString("00");
+            cntlStopWatchOutput.txtSS.Text = _viewModel.CurrentTime.Seconds.ToString("00");
+            cntlStopWatchOutput.txtMS.Text = _viewModel.CurrentTime.Milliseconds.ToString("000");
         }
 
         public void bttnStart_Click(object sender, RoutedEventArgs e)
@@ -73,10 +76,18 @@ namespace StopWatch.Controls
 
         private void bttnPause_Click(object sender, RoutedEventArgs e)
         {
-            lblAusgabe.Content = "00:00:00";
+            //lblAusgabe.Content = "00:00:00";
+            ClearView();
             _viewModel.Stop();
             _timer.Stop();
             bttnStart.IsChecked = false;
+        }
+
+        private void ClearView()
+        {
+            cntlStopWatchOutput.txtMM.Text ="00";
+            cntlStopWatchOutput.txtSS.Text ="00";
+            cntlStopWatchOutput.txtMS.Text = "00";
         }
 
         private void bttnShow_Click(object sender, RoutedEventArgs e)
---
+++ StopWatch/Controls/ControlStopWatchOutput.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StopWatch.Controls
+{
+    /// <summary>
+    /// Interaktionslogik für ControlStopWatchOutput.xaml
+    /// </summary>
+    public partial class ControlStopWatchOutput
+    {
+        public ControlStopWatchOutput()
+        {
+            InitializeComponent();
+            txtMM.Text = "00";
+            txtSS.Text = "00";
+            txtMS.Text = "00";
+        }
+    }
+}
